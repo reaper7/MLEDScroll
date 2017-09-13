03b7977f77bd5ebe70fd0345ad4daba7af543c5b
reaper7 <reaper7md@gmail.com>
2017-09-13 08:35:30 +0200

    update examples/simple_dots/simple_dots.ino

--- examples/simple_dots/simple_dots.ino
+++ examples/simple_dots/simple_dots.ino
@@ -9,28 +9,19 @@ void setup() {
 
 void loop() {
   matrix.dot(0, 0, 1);
-  matrix.dot(7, 0, 0);
-  matrix.dot(0, 7, 0);
-  matrix.dot(7, 7, 0);
   matrix.display();
   delay(500);
 
   matrix.dot(0, 0, 0);
   matrix.dot(7, 0, 1);
-  matrix.dot(0, 7, 0);
-  matrix.dot(7, 7, 0);
   matrix.display();
   delay(500);
 
-  matrix.dot(0, 0, 0);
   matrix.dot(7, 0, 0);
   matrix.dot(0, 7, 1);
-  matrix.dot(7, 7, 0);
   matrix.display();
   delay(500);
-  
-  matrix.dot(0, 0, 0);
-  matrix.dot(7, 0, 0);
+
   matrix.dot(0, 7, 0);
   matrix.dot(7, 7, 1);
   matrix.display();
